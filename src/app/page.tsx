17c4fa6b763cfd0b4124e5f2e5259cbac48276f0
Shivam <gautamshivam043@gmail.com>
2024-07-25 23:28:20 +0530

    add acertinitty ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Banner from "../../components/banner";
 import Header from "../../components/header";
+import { HeroParallaxDemo } from "../../components/heroParallax";
 import Navbar from "../../components/navbar";
+import { HeroParallax } from "../../components/ui/hero-parallax";
 
 export default function Home() {
     return (
         <>
-            <Navbar/>
-            <Header/>
+            <Navbar />
+            <Header />
             <Banner />
+            <HeroParallaxDemo />
         </>
     );
 }
